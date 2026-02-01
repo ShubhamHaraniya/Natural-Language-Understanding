380e7ca33ef72b0f34147cc5a98f22395e14ce0b
Shubham Haraniya <109221112+ShubhamHaraniya@users.noreply.github.com>
2026-02-02 00:45:41 +0530

    Cleanup: Remove all files except docs and M25CSA013_prob4.py

--- Assignment 1/check_env.py
+++
@@ -1,9 +0,0 @@
-try:
-    import sklearn
-    from sklearn.datasets import fetch_20newsgroups
-    import seaborn
-    import matplotlib
-    import wordcloud
-    print("Imports success")
-except Exception as e:
-    print(f"Import failed: {e}")
